b7dcc21787cc08c0647fb3b9c82c41b31552f267
Robin Schmidt <schmidt.rbn@gmail.com>
2022-09-20 13:27:41 +0200

    initial commit

---
+++ db.js
@@ -0,0 +1,7 @@
+const { Pool } = require('pg');
+
+const connectionString = process.env.PG_CONNECTIONSTRING;
+
+const pool = new Pool({ connectionString });
+
+module.exports = pool;
---
+++ server.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const pool = require('./db');
+//pg is just for testing
+const pg = require('pg');
+
+const PORT = process.envPORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+const conString = process.env.PG_CONNECTIONSTRING;
+const client = new pg.Client(conString);
+client.connect(function (err) {
+  if (err) {
+    return console.error('could not connect to postgres', err);
+  }
+  client.query('SELECT NOW() AS "theTime"', function (err, result) {
+    if (err) {
+      return console.error('error running query', err);
+    }
+    console.log(result.rows[0].theTime);
+    // >> output: 2018-08-23T14:02:57.117Z
+    client.end();
+  });
+});
+
+app.listen(PORT, () => console.log(`Server is listening to ${PORT}`));
