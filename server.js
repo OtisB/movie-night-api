a313d7d860dcd5807b2a5ea53e240324df463dfe
Niklas <Niklas-Riebesell@outlook.de>
2022-09-21 20:08:28 +0200

    Route for Movies

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const pool = require('./db');
+const pool = require("./db");
 //pg is just for testing
-const pg = require('pg');
+const pg = require("pg");
 
 const PORT = process.envPORT || 8080;
 
@@ -16,16 +16,26 @@ const conString = process.env.PG_CONNECTIONSTRING;
 const client = new pg.Client(conString);
 client.connect(function (err) {
   if (err) {
-    return console.error('could not connect to postgres', err);
+    return console.error("could not connect to postgres", err);
   }
   client.query('SELECT NOW() AS "theTime"', function (err, result) {
     if (err) {
-      return console.error('error running query', err);
+      return console.error("error running query", err);
     }
     console.log(result.rows[0].theTime);
-    // >> output: 2018-08-23T14:02:57.117Z
+
     client.end();
   });
 });
 
 app.listen(PORT, () => console.log(`Server is listening to ${PORT}`));
+
+app.get("/", async (req, res) => {
+  try {
+    const { rows } = await pool.query("SELECT * FROM movie");
+    console.log(rows);
+    res.json(rows);
+  } catch (err) {
+    console.log(err);
+  }
+});
