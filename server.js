470cd05dbba2c37cb083d6d5eebb255f2f34db1b
Robin Schmidt <schmidt.rbn@gmail.com>
2022-09-22 16:51:23 +0200

    MODIFIED: added start script

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const app = express();
 const pool = require("./db");
 //pg is just for testing
-const pg = require("pg");
+//const pg = require("pg");
 
 const PORT = process.envPORT || 8080;
 
@@ -12,28 +12,28 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
-const conString = process.env.PG_CONNECTIONSTRING;
-const client = new pg.Client(conString);
-client.connect(function (err) {
-  if (err) {
-    return console.error("could not connect to postgres", err);
-  }
-  client.query('SELECT NOW() AS "theTime"', function (err, result) {
-    if (err) {
-      return console.error("error running query", err);
-    }
-    console.log(result.rows[0].theTime);
+//const conString = process.env.PG_CONNECTIONSTRING;
+// const client = new pg.Client(conString);
+// client.connect(function (err) {
+//   if (err) {
+//     return console.error("could not connect to postgres", err);
+//   }
+//   client.query('SELECT NOW() AS "theTime"', function (err, result) {
+//     if (err) {
+//       return console.error("error running query", err);
+//     }
+//     console.log(result.rows[0].theTime);
 
-    client.end();
-  });
-});
+//     client.end();
+//   });
+// });
 
 app.listen(PORT, () => console.log(`Server is listening to ${PORT}`));
 
 app.get("/", async (req, res) => {
   try {
     const { rows } = await pool.query("SELECT * FROM movie");
-    console.log(rows);
+    //console.log(rows);
     res.json(rows);
   } catch (err) {
     console.log(err);
